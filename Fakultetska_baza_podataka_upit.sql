7ead0fe2b6671236291838360e2612f740ac9c75
Александар Стефановић <stefanovicsalex@gmail.com>
2023-08-30 22:23:38 +0200

    Креирање базе података
    Овде сам схватио да NVARCHAR не може да узме 50000 карактера

---
+++ Fakultetska_baza_podataka_upit.sql
@@ -0,0 +1,12 @@
+﻿CREATE DATABASE Fakultetska_baza_podataka;
+USE Fakultetska_baza_podataka;
+
+//----------------------------------------------
+CREATE TABLE Predmet(
+  id INT PRIMARY KEY,
+  naziv NVARCHAR(100) NOT NULL,
+  godina INT NOT NULL,
+  semestar INT NOT NULL,
+  istorijat NVARCHAR(50),
+  
+);
