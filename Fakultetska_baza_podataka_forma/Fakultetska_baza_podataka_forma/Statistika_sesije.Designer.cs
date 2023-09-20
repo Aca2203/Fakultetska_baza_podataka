d8e1c7c455ed6dbac66f515629cc8feeee99b8b8
Александар Стефановић <stefanovicsalex@gmail.com>
2023-09-20 14:10:28 +0200

    Форма ,,Статистика за сесије''
    Статистика по предметима
    Битно!
    Уклонити предмет ,,Непознато'' и уместо тога креирати табелу ,,Датум''!

--- Fakultetska_baza_podataka_forma/Fakultetska_baza_podataka_forma/Statistika_sesije.Designer.cs
+++ Fakultetska_baza_podataka_forma/Fakultetska_baza_podataka_forma/Statistika_sesije.Designer.cs
@@ -28,11 +28,11 @@
         /// </summary>
         private void InitializeComponent()
         {
-            System.Windows.Forms.DataVisualization.Charting.ChartArea chartArea1 = new System.Windows.Forms.DataVisualization.Charting.ChartArea();
-            System.Windows.Forms.DataVisualization.Charting.Legend legend1 = new System.Windows.Forms.DataVisualization.Charting.Legend();
-            System.Windows.Forms.DataVisualization.Charting.Series series1 = new System.Windows.Forms.DataVisualization.Charting.Series();
-            System.Windows.Forms.DataVisualization.Charting.Series series2 = new System.Windows.Forms.DataVisualization.Charting.Series();
-            System.Windows.Forms.DataVisualization.Charting.Title title1 = new System.Windows.Forms.DataVisualization.Charting.Title();
+            System.Windows.Forms.DataVisualization.Charting.ChartArea chartArea2 = new System.Windows.Forms.DataVisualization.Charting.ChartArea();
+            System.Windows.Forms.DataVisualization.Charting.Legend legend2 = new System.Windows.Forms.DataVisualization.Charting.Legend();
+            System.Windows.Forms.DataVisualization.Charting.Series series3 = new System.Windows.Forms.DataVisualization.Charting.Series();
+            System.Windows.Forms.DataVisualization.Charting.Series series4 = new System.Windows.Forms.DataVisualization.Charting.Series();
+            System.Windows.Forms.DataVisualization.Charting.Title title2 = new System.Windows.Forms.DataVisualization.Charting.Title();
             this.grafikon = new System.Windows.Forms.DataVisualization.Charting.Chart();
             this.lbl_ukupno_vreme_ucenja = new System.Windows.Forms.Label();
             this.labela = new System.Windows.Forms.Label();
@@ -42,44 +42,46 @@
             this.txt_efektivno_vreme = new System.Windows.Forms.TextBox();
             this.txt_efikasnost = new System.Windows.Forms.TextBox();
             this.btn_osvezi = new System.Windows.Forms.Button();
+            this.cmb_predmet = new System.Windows.Forms.ComboBox();
+            this.lbl_predmet = new System.Windows.Forms.Label();
             ((System.ComponentModel.ISupportInitialize)(this.grafikon)).BeginInit();
             this.SuspendLayout();
             // 
             // grafikon
             // 
-            chartArea1.AxisY.Minimum = 0D;
-            chartArea1.Name = "ChartArea1";
-            this.grafikon.ChartAreas.Add(chartArea1);
-            legend1.Name = "Legend1";
-            this.grafikon.Legends.Add(legend1);
+            chartArea2.AxisY.Minimum = 0D;
+            chartArea2.Name = "ChartArea1";
+            this.grafikon.ChartAreas.Add(chartArea2);
+            legend2.Name = "Legend1";
+            this.grafikon.Legends.Add(legend2);
             this.grafikon.Location = new System.Drawing.Point(12, 237);
             this.grafikon.Name = "grafikon";
-            series1.ChartArea = "ChartArea1";
-            series1.Color = System.Drawing.Color.Green;
-            series1.IsValueShownAsLabel = true;
-            series1.Legend = "Legend1";
-            series1.Name = "Укупно време учења";
-            series1.XValueMember = "Датум и ефикасност";
-            series1.XValueType = System.Windows.Forms.DataVisualization.Charting.ChartValueType.String;
-            series1.YValueMembers = "Укупно време";
-            series1.YValueType = System.Windows.Forms.DataVisualization.Charting.ChartValueType.Time;
-            series2.ChartArea = "ChartArea1";
-            series2.Color = System.Drawing.Color.FromArgb(((int)(((byte)(255)))), ((int)(((byte)(128)))), ((int)(((byte)(0)))));
-            series2.IsValueShownAsLabel = true;
-            series2.Legend = "Legend1";
-            series2.Name = "Ефективно време учења";
-            series2.XValueMember = "Датум и ефикасност";
-            series2.XValueType = System.Windows.Forms.DataVisualization.Charting.ChartValueType.String;
-            series2.YValueMembers = "Ефективно време";
-            series2.YValueType = System.Windows.Forms.DataVisualization.Charting.ChartValueType.Time;
-            this.grafikon.Series.Add(series1);
-            this.grafikon.Series.Add(series2);
+            series3.ChartArea = "ChartArea1";
+            series3.Color = System.Drawing.Color.Green;
+            series3.IsValueShownAsLabel = true;
+            series3.Legend = "Legend1";
+            series3.Name = "Укупно време учења";
+            series3.XValueMember = "Датум и ефикасност";
+            series3.XValueType = System.Windows.Forms.DataVisualization.Charting.ChartValueType.String;
+            series3.YValueMembers = "Укупно време";
+            series3.YValueType = System.Windows.Forms.DataVisualization.Charting.ChartValueType.Time;
+            series4.ChartArea = "ChartArea1";
+            series4.Color = System.Drawing.Color.FromArgb(((int)(((byte)(255)))), ((int)(((byte)(128)))), ((int)(((byte)(0)))));
+            series4.IsValueShownAsLabel = true;
+            series4.Legend = "Legend1";
+            series4.Name = "Ефективно време учења";
+            series4.XValueMember = "Датум и ефикасност";
+            series4.XValueType = System.Windows.Forms.DataVisualization.Charting.ChartValueType.String;
+            series4.YValueMembers = "Ефективно време";
+            series4.YValueType = System.Windows.Forms.DataVisualization.Charting.ChartValueType.Time;
+            this.grafikon.Series.Add(series3);
+            this.grafikon.Series.Add(series4);
             this.grafikon.Size = new System.Drawing.Size(1577, 730);
             this.grafikon.TabIndex = 0;
             this.grafikon.Text = "chart1";
-            title1.Name = "Title1";
-            title1.Text = "Време учења";
-            this.grafikon.Titles.Add(title1);
+            title2.Name = "Title1";
+            title2.Text = "Време учења";
+            this.grafikon.Titles.Add(title2);
             // 
             // lbl_ukupno_vreme_ucenja
             // 
@@ -151,7 +153,7 @@
             // btn_osvezi
             // 
             this.btn_osvezi.Font = new System.Drawing.Font("Calibri", 18F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-            this.btn_osvezi.Location = new System.Drawing.Point(761, 96);
+            this.btn_osvezi.Location = new System.Drawing.Point(766, 115);
             this.btn_osvezi.Name = "btn_osvezi";
             this.btn_osvezi.Size = new System.Drawing.Size(252, 46);
             this.btn_osvezi.TabIndex = 41;
@@ -159,11 +161,32 @@
             this.btn_osvezi.UseVisualStyleBackColor = true;
             this.btn_osvezi.Click += new System.EventHandler(this.btn_osvezi_Click);
             // 
+            // cmb_predmet
+            // 
+            this.cmb_predmet.Font = new System.Drawing.Font("Calibri", 18F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.cmb_predmet.FormattingEnabled = true;
+            this.cmb_predmet.Location = new System.Drawing.Point(663, 8);
+            this.cmb_predmet.Name = "cmb_predmet";
+            this.cmb_predmet.Size = new System.Drawing.Size(621, 37);
+            this.cmb_predmet.TabIndex = 43;
+            // 
+            // lbl_predmet
+            // 
+            this.lbl_predmet.AutoSize = true;
+            this.lbl_predmet.Font = new System.Drawing.Font("Calibri", 18F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.lbl_predmet.Location = new System.Drawing.Point(548, 11);
+            this.lbl_predmet.Name = "lbl_predmet";
+            this.lbl_predmet.Size = new System.Drawing.Size(109, 29);
+            this.lbl_predmet.TabIndex = 42;
+            this.lbl_predmet.Text = "Предмет:";
+            // 
             // Statistika_sesije
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
             this.ClientSize = new System.Drawing.Size(1601, 979);
+            this.Controls.Add(this.cmb_predmet);
+            this.Controls.Add(this.lbl_predmet);
             this.Controls.Add(this.btn_osvezi);
             this.Controls.Add(this.txt_efikasnost);
             this.Controls.Add(this.txt_efektivno_vreme);
@@ -193,5 +216,7 @@
         private System.Windows.Forms.TextBox txt_efektivno_vreme;
         private System.Windows.Forms.TextBox txt_efikasnost;
         private System.Windows.Forms.Button btn_osvezi;
+        private System.Windows.Forms.ComboBox cmb_predmet;
+        private System.Windows.Forms.Label lbl_predmet;
     }
 }
--- Fakultetska_baza_podataka_forma/Fakultetska_baza_podataka_forma/Statistika_sesije.cs
+++ Fakultetska_baza_podataka_forma/Fakultetska_baza_podataka_forma/Statistika_sesije.cs
@@ -18,6 +18,7 @@ namespace Fakultetska_baza_podataka_forma
         SqlConnection veza;
         string CS = ConfigurationManager.ConnectionStrings["CS"].ToString();
         DataTable tabela = new DataTable();
+        DataTable predmeti = new DataTable();
 
         public Statistika_sesije()
         {
@@ -26,20 +27,34 @@ namespace Fakultetska_baza_podataka_forma
 
         private void Osvezi()
         {
-            tabela.Clear();
+            tabela.Clear();            
+            SqlDataAdapter adapter;            
             veza = new SqlConnection(CS);
-            SqlDataAdapter adapter = new SqlDataAdapter("SELECT DISTINCT CONVERT(VARCHAR, datum, 104) + CHAR(13) + CHAR(10) + " +
+            veza.Open();
+            SqlCommand komanda = new SqlCommand("SELECT id FROM Predmet WHERE naziv = N'Непознато'", veza);
+            if (cmb_predmet.SelectedValue == komanda.ExecuteScalar())
+            {
+                MessageBox.Show("Usao");
+                adapter = new SqlDataAdapter("SELECT DISTINCT CONVERT(VARCHAR, datum, 104) + CHAR(13) + CHAR(10) + " +
                 "CAST(dbo.efikasnost_sesije(dbo.minuti_u_sate(SUM(dbo.sati_u_minute(ukupno_vreme))), dbo.minuti_u_sate(SUM(dbo.sati_u_minute(efektivno_vreme)))) AS " +
                 "VARCHAR) + '%' AS 'Датум и ефикасност', CAST(dbo.minuti_u_sate(SUM(dbo.sati_u_minute(ukupno_vreme))) AS VARCHAR(5)) AS 'Укупно време', " +
                 "CAST(dbo.minuti_u_sate(SUM(dbo.sati_u_minute(efektivno_vreme))) AS VARCHAR(5)) AS 'Ефективно време' FROM Sesija WHERE " +
-                "datum >= DATEADD(DAY, -7, CAST(GETDATE() AS DATE)) AND datum <= CAST(GETDATE() AS DATE) GROUP BY datum;", veza);
+                "datum >= DATEADD(DAY, -6, CAST(GETDATE() AS DATE)) AND datum <= CAST(GETDATE() AS DATE) GROUP BY datum;", veza);
+            }
+            else
+            {
+                adapter = new SqlDataAdapter("SELECT DISTINCT CONVERT(VARCHAR, datum, 104) + CHAR(13) + CHAR(10) + " +
+                "CAST(dbo.efikasnost_sesije(dbo.minuti_u_sate(SUM(dbo.sati_u_minute(ukupno_vreme))), dbo.minuti_u_sate(SUM(dbo.sati_u_minute(efektivno_vreme)))) AS " +
+                "VARCHAR) + '%' AS 'Датум и ефикасност', CAST(dbo.minuti_u_sate(SUM(dbo.sati_u_minute(ukupno_vreme))) AS VARCHAR(5)) AS 'Укупно време', " +
+                "CAST(dbo.minuti_u_sate(SUM(dbo.sati_u_minute(efektivno_vreme))) AS VARCHAR(5)) AS 'Ефективно време' FROM Sesija WHERE " +
+                "datum >= DATEADD(DAY, -6, CAST(GETDATE() AS DATE)) AND datum <= CAST(GETDATE() AS DATE) AND (fk_predmet = " + cmb_predmet.SelectedValue + " OR fk_predmet = " + komanda.ExecuteScalar() + ") GROUP BY datum;", veza);
+            }
             adapter.Fill(tabela);
             grafikon.DataSource = tabela;    
             grafikon.DataBind();                     
-            SqlCommand komanda = new SqlCommand("SELECT dbo.minuti_u_sate(SUM(dbo.sati_u_minute(ukupno_vreme))) FROM Sesija WHERE datum >= DATEADD(DAY, -7, CAST(GETDATE() AS DATE)) AND datum <= CAST(GETDATE() AS DATE);", veza);
-            veza.Open();
+            komanda = new SqlCommand("SELECT dbo.minuti_u_sate(SUM(dbo.sati_u_minute(ukupno_vreme))) FROM Sesija WHERE datum >= DATEADD(DAY, -6, CAST(GETDATE() AS DATE)) AND datum <= CAST(GETDATE() AS DATE);", veza);            
             txt_ukupno_vreme.Text = komanda.ExecuteScalar().ToString();
-            komanda = new SqlCommand("SELECT dbo.minuti_u_sate(SUM(dbo.sati_u_minute(efektivno_vreme))) FROM Sesija WHERE datum >= DATEADD(DAY, -7, CAST(GETDATE() AS DATE)) AND datum <= CAST(GETDATE() AS DATE);", veza);
+            komanda = new SqlCommand("SELECT dbo.minuti_u_sate(SUM(dbo.sati_u_minute(efektivno_vreme))) FROM Sesija WHERE datum >= DATEADD(DAY, -6, CAST(GETDATE() AS DATE)) AND datum <= CAST(GETDATE() AS DATE);", veza);
             txt_efektivno_vreme.Text = komanda.ExecuteScalar().ToString();
             komanda = new SqlCommand("SELECT dbo.efikasnost('" + txt_ukupno_vreme.Text + "', '" + txt_efektivno_vreme.Text + "')", veza);
             txt_efikasnost.Text = komanda.ExecuteScalar().ToString() + "%";
@@ -47,7 +62,17 @@ namespace Fakultetska_baza_podataka_forma
         }
 
         private void Statistika_sesije_Load(object sender, EventArgs e)
-        {
+        {            
+            veza = new SqlConnection(CS);
+            veza.Open();
+            SqlDataAdapter adapter = new SqlDataAdapter("SELECT * FROM Predmet", veza);            
+            adapter.Fill(predmeti);
+            cmb_predmet.DataSource = predmeti;
+            cmb_predmet.ValueMember = "id";
+            cmb_predmet.DisplayMember = "naziv";
+            SqlCommand komanda = new SqlCommand("SELECT id FROM Predmet WHERE naziv = N'Непознато'", veza);
+            cmb_predmet.SelectedValue = komanda.ExecuteScalar();
+            veza.Close();
             Osvezi();
         }
 
